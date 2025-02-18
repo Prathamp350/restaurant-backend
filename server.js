08886dbdedf28ca9c39832e01d40c9e7d5faea76
Prathamp350 <Prathampatel350@gmail.com>
2025-02-18 12:19:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect('mongodb+srv://Pratham:Hkyzza5h93*@cluster0.ze1cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', )
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Pratham:Hkyzza5h93*@cluster0.ze1cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('Could not connect to MongoDB:', err);
@@ -396,4 +397,7 @@ app.get('/api/orders/:orderId', async (req, res) => {
 
 
 // Start the Server
-app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
